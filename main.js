ae9b9473dec549160fae7e9ed979f2e3d5ce4064
Amir A. Shabani <amirshabani011@gmail.com>
2019-12-23 11:37:26 +0330

    Hello World, works

---
+++ main.js
@@ -0,0 +1,33 @@
+let {app, BrowserWindow} = require("electron");
+
+let main_window;
+
+function create_main_window() {
+	main_window = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+
+	main_window.loadFile("index.html");
+	main_window.on("closed", () => {
+		main_window = null;
+	});
+
+}
+
+app.on("ready", create_main_window);
+
+app.on("window-all-closed", () => {
+	if(process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if(main_window === null) {
+		create_main_window();
+	}
+});
